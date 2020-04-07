22e3ea78da1b3995d513eb209a81792963852790
Annaruth McBride <armcbride17@gmail.com>
2020-04-07 17:36:27 -0500

    finished questions

--- Develop/index.js
+++ Develop/index.js
@@ -6,10 +6,55 @@ inquirer
 
 const questions = [
 {type: "input",
-message: "what is your username?",
-name: "user",
+message: "what is your name?",
+name: "name"
 },
-
+{type: "input",
+message: "what is your Github username?",
+name: "github"
+},
+{type: "input",
+message: "what is your Project Name?",
+name: "project"
+},
+{type: "input",
+message: "Describe your project.",
+name: "description"
+},
+{type: "input",
+message: "What sort of installation is necessary?",
+name: "install"
+},
+{type: "checkbox",
+message: "what technologies did you use?",
+name: "technology",
+choices: ["HTML", "CSS","javaScript","jQuery", "AJAX","Node.JS","Express","React.js", "React", "MongoDB", "Heroku", "Command-Line", "Bootstrap", "MySQL"]
+},
+{type: "input",
+message: "What is the purpose of this app?",
+name: "usage"
+},
+{type: "list",
+message: "What license is used?",
+choices: ["MIT", "BSD", "ISC", "Apache", "GPL"],
+name: "list"
+},
+{type: "input",
+message: "Who contributed?",
+name: "contributors"
+},
+{type: "input",
+message: "What is your linkedIn username?",
+name: "linkedin"
+},
+{type: "input",
+message: "What is the link to your portfolio?",
+name: "portfolio"
+},
+{type: "input",
+message: "Tests?",
+name: "tests"
+},
 ];
 
 function writeToFile(fileName, data) {
