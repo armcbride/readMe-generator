7e1c05733cc98225c7588f84bda64e77192643d3
Annaruth McBride <armcbride17@gmail.com>
2020-04-09 13:26:06 -0500

    adjusting questions and reordering page

---
+++ Develop/create-README.md
@@ -0,0 +1,34 @@
+
+  <img align="left" src= "https://img.shields.io/badge/License-MIT-green">
+  <h1 align= "center">readme-generator</h1> 
+  <div># Table of Contents </div>
+  <ul>
+  <li><a href="#description">Description</a></li>  
+  <li><a href="#installation">Installation</a></li> 
+  <li><a href="#tech">Technology</a></li> 
+  <li><a href="#usage">Usage</a></li> 
+  <li><a href="#screen">ScreenShots</a></li> 
+  <li><a href="#contributors">Contributors</a></li>   
+  <li><a href="#contact">Contact</a></li> 
+  <li><a href="#tests">Tests</a></li> 
+  </ul>
+  <div id="description"># Description </div>
+  test
+  <div id="installation"># Installation </div>
+  <p>test          
+  <div id="tech"># Technology</div>           
+  <p>Node.Js</p>          
+  <div id="usage"># Usage </div>
+  <p>test</p>   
+  <div id="screen"># Screenhots </div>
+  <div id="contributors"># Contributors </div>
+  <p>test</p> 
+  <div id="contact"># Contact </div>
+  <img align="right" width="100" height="100" src="https://avatars3.githubusercontent.com/u/58277359?v=4">         
+Name: Annaruth McBride       
+  <a href= "https://github.com/armcbride">GitHub</a>
+  <a href= "test">Portfolio</a>
+  <a href= "mailto:armcbride17@gmail.com">armcbride17@gmail.com</a>      
+  <a href= "https://www.linkedin.com/in/test">LinkedIn</a> 
+  <div id="tests">Tests</div>
+  <p>test</p>
--- Develop/index.js
+++ Develop/index.js
@@ -7,33 +7,33 @@ function getQuestions() {
     inquirer
       .prompt([{
           type: "input",
-          message: "What is your full name?",
+          message: "What is your name?",
           name: "name"
         },
         {
           type: "input",
-          message: "GitHub username",
+          message: "What is your GitHub username?",
           name: "username"
         },
         {
           type: "Input",
-          message: "Project Name",
+          message: "What is the project name?",
           name: "project"
         },
         {
           type: "input",
-          message: "Description",
+          message: "Describe the application:",
           name: "description"
         },
         {
           type: "input",
-          message: "What do you need to install to make this app to work?",
+          message: "What needs to be installed to make this function?",
           name: "installation"
         },
         {
           type: "checkbox",
-          message: "Technology Used",
-          choices: ["Node.Js", " Express", " JavaScript", " jQuery", " React.js", " React", " GIT", " GitHub", " MongoDB", " MySQL", " Firebase", " Handlebars", " HTML", " CSS", " Bootstrap", " Media Queries", " APIs", " Microsoft Suite", " Heroku", " Command- Line"],
+          message: "Technology used",
+          choices: ["Node.Js", " Express", " JavaScript", " jQuery", " React.js", " React", " MongoDB", " MySQL", " HTML", " CSS", " Bootstrap", " APIs", " Heroku", " Command- Line"],
           name: "technology"
         },
         {
@@ -49,7 +49,7 @@ function getQuestions() {
         },
         {
           type: "input",
-          message: "Contributors",
+          message: "Who contributed in the project?",
           name: "contributors"
         },
         {
@@ -66,6 +66,11 @@ function getQuestions() {
           type: "input",
           message: "Tests?",
           name: "tests"
+        },
+        {
+          type: "input",
+          message: "Any Questions?",
+          name: "questions"
         }
       ])
       .then(function (response) {
@@ -103,7 +108,6 @@ function getQuestions() {
   function generateMarkdown(response, res) {
   
     const usersInfo = `
-  <img align="left" src= "https://img.shields.io/badge/License-${response.license}-green">
   <h1 align= "center">${response.project}</h1> 
   <div># Table of Contents </div>
   <ul>
@@ -111,25 +115,28 @@ function getQuestions() {
   <li><a href="#installation">Installation</a></li> 
   <li><a href="#tech">Technology</a></li> 
   <li><a href="#usage">Usage</a></li> 
+  <li><a href="#license">License</a></li>
   <li><a href="#screen">ScreenShots</a></li> 
   <li><a href="#contributors">Contributors</a></li>   
   <li><a href="#contact">Contact</a></li> 
   <li><a href="#tests">Tests</a></li> 
   </ul>
-  <div id="description"># Description </div>
+  <div id="description"><h3>Description</h3> </div>
   ${response.description}
-  <div id="installation"># Installation </div>
+  <div id="installation"><h3>Installation </div>
   <p>${response.installation}          
-  <div id="tech"># Technology</div>           
+  <div id="tech"><h3>Technology</div>           
   <p>${response.technology}</p>          
-  <div id="usage"># Usage </div>
+  <div id="usage"><h3>Usage </div>
   <p>${response.usage}</p>   
-  <div id="screen"># Screenhots </div>
-  <div id="contributors"># Contributors </div>
+  <div id="license"><h3>License</div>
+  <p><img align="left" src= "https://img.shields.io/badge/License-${response.license}-blue"></p>
+  <div id="screen"><h3>Screenhots </div>
+  <div id="contributors"><h3>Contributors </div>
   <p>${response.contributors}</p> 
-  <div id="contact"># Contact </div>
+  <div id="contact"><h3>Contact </div>
   <img align="right" width="100" height="100" src="${res.data.avatar_url}">         
-Name: ${response.name}       
+    Name: ${response.name}       
   <a href= "https://github.com/${response.username}">GitHub</a>
   <a href= "${response.portfolio}">Portfolio</a>
   <a href= "mailto:${res.data.email}">${res.data.email}</a>      
